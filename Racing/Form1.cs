0edb382080697745438d04864462bebb8016f49d
snklim <sergey.klimenko@gmail.com>
2013-08-01 17:57:34 +0300

    Racing

---
+++ Racing/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Racing
+{
+    public partial class Form1 : Form
+    {
+        private Timer m_MainTimer;
+        private GameField m_GameField;
+
+        public Form1()
+        {
+            m_GameField = new GameField(30, 60);
+            
+            InitializeComponent();
+
+            InitGameFieldEnviroment();
+
+            m_MainTimer = new Timer();
+            m_MainTimer.Interval = 10;
+            m_MainTimer.Tick += m_MainTimer_Tick;
+            m_MainTimer.Start();
+        }
+
+        void m_MainTimer_Tick(object sender, EventArgs e)
+        {
+            m_GameField.Tick();
+            pictureBox1.Refresh();
+        }
+
+        int m_CellSize, m_DeltaX, m_DeltaY, m_CanvasWidth, m_CanvasHeight;
+        void InitGameFieldEnviroment()
+        {
+            m_CanvasWidth = pictureBox1.Width;
+            m_CanvasHeight = pictureBox1.Height;
+
+            m_CellSize = m_CanvasWidth / m_GameField.Width;
+            m_CellSize = Math.Min(m_CellSize, m_CanvasHeight / m_GameField.Height);
+
+            m_DeltaX = (m_CanvasWidth - m_CellSize * m_GameField.Width) / 2;
+            m_DeltaY = (m_CanvasHeight - m_CellSize * m_GameField.Height) / 2;
+        }
+
+        void DrawGameField(Graphics g)
+        {
+            g.Clear(Color.Gray);
+
+            for (int x = 1; x < m_GameField.Width - 1; x++)
+            {
+                for (int y = 1; y < m_GameField.Height - 1; y++)
+                {
+                    if (m_GameField.GetValue(x, y) == 0)
+                        g.DrawRectangle(new Pen(Color.Silver), m_DeltaX + x * m_CellSize, m_DeltaY + y * m_CellSize, m_CellSize, m_CellSize);
+                    else if (m_GameField.GetValue(x, y) == 1)
+                        g.FillRectangle(new SolidBrush(Color.Red), m_DeltaX + x * m_CellSize, m_DeltaY + y * m_CellSize, m_CellSize, m_CellSize);
+                    else if (m_GameField.GetValue(x, y) == 2)
+                        g.FillRectangle(new SolidBrush(Color.Orange), m_DeltaX + x * m_CellSize, m_DeltaY + y * m_CellSize, m_CellSize, m_CellSize);
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawGameField(e.Graphics);
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            InitGameFieldEnviroment();
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case 37: m_GameField.CarMoveLeft(); break;
+                case 38: m_GameField.CarMoveForward(); break;
+                case 39: m_GameField.CarMoveRight(); break;
+                case 40: m_GameField.CarMoveBackward(); break;
+            }
+        }
+    }
+}
---
+++ Racing/GameField.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Racing
+{
+    class GameField
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private int[,] _mainFiled;
+        private int[,] _shadowField;
+
+        private Random _rnd;
+        
+        int _carPosX, _carPosY;
+
+        public GameField(int pWidth, int pHeight)
+        {
+            Width = pWidth;
+            Height = pHeight;
+
+            _mainFiled = new int[Width, Height];
+            _shadowField = new int[Width, Height];
+
+            _carPosX = Width / 2;
+            _carPosY = Height / 2;
+
+            _rnd = new Random();
+        }
+
+        int _numOfTicks = 0;
+        public void Tick()
+        {
+            if (_numOfTicks % 100 == 0)
+            {
+                _shadowField[_rnd.Next(Width - 2) + 1, 1] = 1;
+            }
+
+            if (_numOfTicks % 10 == 0)
+            {
+                for (int x = 1; x < Width - 1; x++)
+                {
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        if (_shadowField[x, y] != 2) _mainFiled[x, y + 1] = _shadowField[x, y];
+                    }
+                }
+
+                for (int x = 1; x < Width - 1; x++)
+                {
+                    for (int y = 1; y < Height - 1; y++)
+                    {
+                        _shadowField[x, y] = _mainFiled[x, y];
+                    }
+                }
+            }
+
+            DrawCar(2);
+
+            _numOfTicks++;
+            if (_numOfTicks == int.MaxValue) _numOfTicks = 0;
+        }
+
+        void DrawCar(int val)
+        {
+            _mainFiled[_carPosX, _carPosY] = val;
+            _mainFiled[_carPosX, _carPosY + 1] = val;
+            _mainFiled[_carPosX - 1, _carPosY + 1] = val;
+            _mainFiled[_carPosX + 1, _carPosY + 1] = val;
+            _mainFiled[_carPosX, _carPosY + 2] = val;
+            _mainFiled[_carPosX, _carPosY + 3] = val;
+            _mainFiled[_carPosX - 1, _carPosY + 3] = val;
+            _mainFiled[_carPosX + 1, _carPosY + 3] = val;
+        }
+
+        public int GetValue(int x, int y)
+        {
+            return _mainFiled[x, y];
+        }
+
+        public void CarMoveLeft()
+        {
+            DrawCar(0);
+            if (_carPosX > 2) _carPosX--;
+        }
+
+        public void CarMoveRight()
+        {
+            DrawCar(0);
+            if (_carPosX < Width - 3) _carPosX++;
+        }
+
+        public void CarMoveForward()
+        {
+            DrawCar(0);
+            if (_carPosY > 1) _carPosY--;
+        }
+
+        public void CarMoveBackward()
+        {
+            DrawCar(0);
+            if (_carPosY < Height - 5) _carPosY++;
+        }
+    }
+}
